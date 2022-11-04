18582e544824df95fff44ce0dc890a4624d13f75
David <95488893+DWAcademProgramming@users.noreply.github.com>
2022-11-04 13:46:34 -0500

    This is a pain in the 🤦‍♂️

---
+++ todo.py
@@ -0,0 +1,9 @@
+user_prompt = "Enter a todo: "
+todo0 = input(user_prompt)
+todo1 = input(user_prompt)
+todo2 = input(user_prompt)
+
+todos = [todo0, todo1, todo2, "Hello "]
+print(todos)
+
+print(type(todos))
