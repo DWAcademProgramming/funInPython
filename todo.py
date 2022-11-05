272c48e2a8bf927683c9b23f53a6261f006fa4a2
gntpldes <h3c6j6@u.northwestern.edu>
2022-11-05 07:46:39 -0500

    Update todo.py

--- todo.py
+++ todo.py
@@ -15,6 +15,10 @@ while True:
             file.writelines(todos)
             file.close()
         case 'show':
+            file = open('todos.txt', 'r')
+            todos = file.readlines()
+            file.close()
+            
             for index, item in enumerate(todos):
                 row = f"{index}-{item}"
                 print(row)
