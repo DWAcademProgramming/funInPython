6a17561666bac133d68ffd6a5cf6f936999bcd05
gntpldes <h3c6j6@u.northwestern.edu>
2022-11-12 13:46:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,19 @@
 import streamlit as sl
+
+sl.set_page_config(layout="wide")
+
+col1, col2 = sl.columns(2)
+
+with col1:
+    sl.image("images/photo.png", width=500)
+
+with col2:
+    sl.title('Random Asshole')
+    content = """
+    This is complete and total bullshit and I'm being laughably childish
+    """
+    comment = """
+        These are some of the projects I've built. 
+    """
+    sl.write(content)
+    sl.write(comment)
