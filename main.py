8eba2da33737d882fd5b9ad5b02ed178469ab955
gntpldes <h3c6j6@u.northwestern.edu>
2022-11-14 12:29:37 -0600

    Something "actually" useful for once...

---
+++ main.py
@@ -0,0 +1,51 @@
+import ssl
+import smtplib
+import requests
+import selectorlib
+
+URL = "http://programmer100.pythonanywhere.com/tours/"
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'
+}
+
+def scrape(url):
+    response = requests.get(url, headers=HEADERS)
+    source = response.text
+    return source
+
+def extract(source):
+    extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
+    value = extractor.extract(source)["tours"]
+    return value
+
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "weslhdavmsu@gmail.com"
+    password = "ylavwroupvtcuomq"
+
+    receiver = "welshdavmsu@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+    print("Email was sent")
+
+def store(extracted):
+    with open("data.txt", "a") as file:
+        file.write(extracted + "\n")
+
+def read(extracted):
+    with open("data.txt", "r") as file:
+        return file.read()
+
+if __name__ == "__main__":
+    scraped = scrape(URL)
+    extracted = extract(scraped)
+    print(extracted)
+    content = read(extracted)
+    if extracted != "No Upcoming Tours":
+        if extracted not in content:
+            store(extracted)
