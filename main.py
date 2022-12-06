375b3c644b9eb5bf0993776bdc2c2fd71ca2ea1e
gntpldes <h3c6j6@u.northwestern.edu>
2022-12-05 20:32:03 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from fpdf import FPDF
+
 class Bill:
     """
     A sample billing application for roommates and their share of a bill
@@ -28,8 +30,19 @@ class pdfReport:
         self.filename = filename
 
     def generate(self, roommate1, roommate2, bill):
-        pass
+        pdf = FPDF(orientation='P', unit='pt', format='A4')
+        pdf.add_page()
+
+        pdf.set_font(family='Times', size=24, style='B')
+        pdf.cell(w=0, h=80, txt='Roommate Bill', border=1, align='C', ln=1)
+        pdf.cell(w=100, h=40, txt='Period: ', border=1)
+        pdf.cell(w=150, h=40, txt=bill.period, border=1, ln=1)
+
+        pdf.output("bill.pdf")
+
 
 the_bill = Bill(amount=120, period= "May 2022")
-max = Roommate(name="Max", time=28)
+john = Roommate(name="Max", time=28)
 jen = Roommate(name="Jen", time=21)
+
+pdf_report = PdfReport(filename="")
